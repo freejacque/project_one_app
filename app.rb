3f7a370dee3897decbbe0e811d2605fc961077f9
freejacque <freejacque@gmail.com>
2014-09-06 01:40:08 -0400

    5 tests from sign_in_spec passed, 1 error

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'pry'
 
 
 class App < Sinatra::Base
@@ -21,14 +22,64 @@ class App < Sinatra::Base
   after do
     logger.info "Response Headers: #{response.headers}"
   end
+  ########################
+  # API Keys
+  ########################
+  CLIENT_ID     = "594095716528-a8lib8gqpnp6o00k23n58c01ev8r5b3d.apps.googleusercontent.com"
+  CLIENT_SECRET = "pQ_Xf9VbZPEcN_wQ5pgNo9X1"
+  EMAIL_ADDRESS = "594095716528-a8lib8gqpnp6o00k23n58c01ev8r5b3d@developer.gserviceaccount.com"
+  CALLBACK_URL  = "http://127.0.0.1:9393/oauth_callback"
+
+
 
   ########################
   # Routes
   ########################
 
   get('/') do
-
+    # binding.pry
+    # if
+      # base_url = "https://accounts.google.com/o/oauth2/auth?"
+      # scope = email%20profile
+      # code  = params[:code]
+      # state = SecureRandom.urlsafe_base64
+      # session[:state] = state
+      # @url  = "#{base_url}scope=#{scope}&state=#{state}&redirect_uri=#{CALLBACK_URL}&response_type=#{code}&client_id=#{CLIENT_ID}&approval_prompt=auto"
     render(:erb, :index)
+  end
+
+  post('/') do
+    # if
+
+
+    # else
+      if params[:user_password] == "password"
+          redirect('/home')
+      else
+          redirect('/password_error')
+      end
+      render(:erb, :home)
+    # end
+  end
+
+  get('/oauth_callback') do
+    # code  = params[:code]
+    # #send a post
+    # if session[:state] == params[:state]
+    #   #send a post
+    # response = HTTParty.post("https://www.googleapis.com/apiName/apiVersion/resourcePath?parameters",
+    #             :body => {
+    #             client_id: CLIENT_ID,
+    #             client_secret: CLIENT_SECRET,
+    #             code: code,
+    #             redirect_uri: CALLBACK_URL,
+    #             },
+    #             :headers => {
+    #               "Accept" => "application/json",
+    #             })
+    #   session[:access_token] = response[:access_token]
+    # end
+    # redirect to('/')
   end
 
   get('/password_error') do
@@ -38,4 +89,9 @@ class App < Sinatra::Base
   get('/sign_up') do
     render(:erb, :sign_up)
   end
+
+  get('/home') do
+    render(:erb, :home)
+  end
+
 end
--- spec/sign_in_spec.rb
+++ spec/sign_in_spec.rb
@@ -20,14 +20,14 @@ describe("Sign in page") do
     it("requests the users email and password") do
       fill_in "user_email", with: "freejacque@gmail.com"
       fill_in "user_password", with: "password"
-      click_on "submit"
+      click_on 'submit'
       expect(page).to have_content("Welcome to Critique-It!")
     end
 
     it("has a link to sign up for an account") do
-      expect(page).to have_content("If you don’t have an account sign up here.")
-      click_on "sign up"
-      expect(page).to have_content("profile pic url:")
+      expect(page).to have_content("If you don’t have an account")
+      click_on 'sign up'
+      expect(page).to have_content("profile pic url")
     end
 
     it("allows the user to sign in using a google account") do
@@ -36,6 +36,7 @@ describe("Sign in page") do
 
     it("allows the user to sign in using a google account") do
       click_on "google account"
+      # other code
       expect(page).to have_content("Welcome to Critique-It!")
     end
   end
